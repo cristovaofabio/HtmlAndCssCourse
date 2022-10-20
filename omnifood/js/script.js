426b0ad4be618e7bf4db7b4d635a439f1c6a6aac
Fabio <cristovaofabio3@gmail.com>
2022-10-20 10:27:51 -0300

    feat: smooth scrolling

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -17,12 +17,38 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-//make mobile navigation work:
+//Make mobile navigation work:
 const headerEl = document.querySelector(".header");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 
 btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
+});
+
+//Smooth scrolling animation:
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
 });
 
 // Fixing flexBox gap property missing in some Safari versions:
